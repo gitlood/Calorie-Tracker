11cdcb92ef1bab19f121eee354b5c3f2b4065d4c
gitlood <unilood@gmail.com>
2022-10-31 16:51:20 +1300

    Route update

--- core/src/main/java/com/plcoding/core/navigation/Route.kt
+++ core/src/main/java/com/plcoding/core/navigation/Route.kt
@@ -9,4 +9,7 @@ object Route {
     const val NUTRIENT_GOAL = "nutrient_goal"
     const val ACTIVITY = "activity"
     const val GOAL = "goal"
+
+    const val TRACKER_OVERVIEW = "tracker_overview"
+    const val SEARCH = "search"
 }
