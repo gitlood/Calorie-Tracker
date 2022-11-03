95464b8015c8b182dbb0e382519f05ab8931ce03
gitlood <unilood@gmail.com>
2022-11-03 15:13:58 +1300

    Interface and Dataclasses created

---
+++ core/src/main/java/com/plcoding/core/domain/preferences/Preferences.kt
@@ -0,0 +1,20 @@
+package com.plcoding.core.domain.preferences
+
+import com.plcoding.core.domain.model.ActivityLevel
+import com.plcoding.core.domain.model.Gender
+import com.plcoding.core.domain.model.GoalType
+import com.plcoding.core.domain.model.UserInfo
+
+interface Preferences {
+    fun saveGender(gender: Gender)
+    fun saveAge(age: Int)
+    fun saveWeight(weight: Float)
+    fun saveHeight(height: Int)
+    fun saveActivityLevel(level: ActivityLevel)
+    fun saveGoalType(type: GoalType)
+    fun saveCarbRatio(ratio: Float)
+    fun saveProteinRatio(ratio: Float)
+    fun saveFatRatio(ratio: Float)
+
+    fun loadUserInfo(): UserInfo
+}
