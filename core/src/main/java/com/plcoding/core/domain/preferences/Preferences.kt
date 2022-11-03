970b1361293183eefbebb5fa12e6c70f329f02e9
gitlood <unilood@gmail.com>
2022-11-03 15:23:33 +1300

    Default Preference update

--- core/src/main/java/com/plcoding/core/domain/preferences/Preferences.kt
+++ core/src/main/java/com/plcoding/core/domain/preferences/Preferences.kt
@@ -17,4 +17,16 @@ interface Preferences {
     fun saveFatRatio(ratio: Float)
 
     fun loadUserInfo(): UserInfo
+
+    companion object {
+        const val KEY_GENDER = "gender"
+        const val KEY_AGE = "age"
+        const val KEY_WEIGHT = "weight"
+        const val KEY_HEIGHT = "height"
+        const val KEY_ACTIVITY_LEVEL = "activity_level"
+        const val KEY_GOAL_TYPE = "goal_type"
+        const val KEY_CARB_RATIO = "carb_ratio"
+        const val KEY_PROTEIN_RATIO = "protein_ratio"
+        const val KEY_FAT_RATIO = "fat_ratio"
+    }
 }
