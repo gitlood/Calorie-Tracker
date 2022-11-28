718cc4ec4d2fb8d4bb9bd0d1aa88f5501bbde4d7
gitlood <unilood@gmail.com>
2022-11-28 16:42:49 +1300

    Tidy up

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -5,10 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
